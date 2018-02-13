d32ea948c92aee93f633a1487a3ce6ef05e633e3
Thomas Casady <tscasady@gmail.com>
2018-02-13 18:59:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 function shout(string){
   return string.toUpperCase()
 }
+
+function whisper(string){
+  return string.
+}
+
+function logShout(string){
+  newString = string.toUpperCase()
+  console.log(newString)
+}
